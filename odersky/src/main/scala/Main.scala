6bb086a405c10c2e2da9e4e28e083c5b72c0ab1f
alcherniaev <playtowin19@gmail.com>
2020-12-28 18:05:31 +0300

    max and main

---
+++ odersky/src/main/scala/Main.scala
@@ -0,0 +1,3 @@
+object Main extends App {
+  println(first_steps_chapter.max(3, 5))
+}
---
+++ odersky/src/main/scala/first_steps_chapter.scala
@@ -0,0 +1,15 @@
+object first_steps_chapter {
+
+  // functions
+  def max(x: Int, y: Int): Int = {
+    if (x > y) x
+    else y
+  }
+
+  // if we don't use recursion, we can write func without defining an output type
+  def max_1(x: Int, y: Int) = if (x > y) x else y
+
+
+
+
+}
