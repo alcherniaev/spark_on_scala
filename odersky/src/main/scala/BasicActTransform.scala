b9155de969e1f0b18415db233d8d9b38a65e8629
alcherniaev <playtowin19@gmail.com>
2021-01-12 11:00:40 +0300

    prints RDD throught collect() and take(n)

--- odersky/src/main/scala/BasicActTransform.scala
+++ odersky/src/main/scala/BasicActTransform.scala
@@ -13,6 +13,10 @@ object BasicActTransform {
     // sparkContext exists in sparkSessions, that defined in val spark
     val num = spark.sparkContext.parallelize(List(1, 2, 3))
 
+    // prints each element of RDD
+    //prints 1 2 3 on separate line
+    num.collect().foreach(println) // collect is not a good idea, though, when the RDD has billions of lines.
+    num.take(3).foreach(println) // use take() to take just a few to print out:
   }
 
 
